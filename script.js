3d4b1fa77d61a3093b35778c639f640ab6c45ee2
CoyotiDog <jeremymonette67@gmail.com>
2025-04-24 20:46:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var clicks = 0;
+var level = 1;
+var cost = 10;
+
+//setup the initial display
+
+document.getElementById("lv").innerText = ("Level " + level);
+document.getElementById("upgrader").innerText = ("Cost " + cost);
+
+// Increments clicks based on the current level and updates the display
+
+function addclick() {
+    clicks += level;
+    document.getElementById("clicker").innerText = ("Clicks " + clicks);
+}
+
+// Handles upgrading the level if sufficient clicks are available
+
+function upgrade() {
+    if (clicks >= cost || clicks == cost) {
+        clicks -= cost;
+        level++;
+        cost *= 2;
+        document.getElementById("upgrader").innerText = ("Cost " + cost);
+        document.getElementById("lv").innerText = ("Level " + level);
+        document.getElementById("clicker").innerText = ("Clicks " + clicks);
+    } else {
+        alert("Not enough clicks!");
+    }
+}
